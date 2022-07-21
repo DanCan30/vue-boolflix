d94d10ddf562af7a5442e0cc365a726a19008a16
DanCan30 <danielecan3008@gmail.com>
2022-07-21 13:04:59 +0200

    Base API call done

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+
+    <Header @search="getFilmsList" />
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+
+  data: function() {
+    return {
+      apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=1003857666890380902d6e3595e8622a&query="
+    }
+  },
+
+  components: {
+    Header,
+    Main,
+  },
+  methods: {
+    getFilmsList: function(search) {
+      axios.get(`${this.apiUrl}${search}`)
+      .then((result) => 
+      console.log(result.data.results));
+    }
+  },
+
+  created() {
+    this.getFilmsList();
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    
+    <div>
+        <input type="text" v-model="searchInput">
+        <button @click="$emit('search', searchInput)">Cerca</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+    data: function() {
+        return {
+            searchInput: "",
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
