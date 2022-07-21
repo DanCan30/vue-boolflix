eeb7e04711fa22a3e68bf8463f2cca86a3ede74d
DanCan30 <danielecan3008@gmail.com>
2022-07-21 22:23:32 +0200

    Layout done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,14 @@
 <template>
     
-    <div>
-        <input type="text" v-model.trim="searchInput" @keyup.enter="emitSearchResult()">
-        <button @click="emitSearchResult()">Cerca</button>
-    </div>
+    <header>
+
+        <h1>netflix</h1>
+
+        <div class="search">
+            <input type="text" v-model.trim="searchInput" @keyup.enter="emitSearchResult()">
+            <i class="fa-solid fa-magnifying-glass" @click="emitSearchResult()"></i>
+        </div>
+    </header>
 
 </template>
 
@@ -26,6 +31,61 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+    header {
+        height: 5rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 2rem 1rem;
+
+        h1 {
+            color: red;
+            text-transform: uppercase;
+            font-size: 4rem;
+            position: relative;
+
+            &::after {
+                content: "netflix";
+                text-transform: uppercase;
+                position: absolute;
+                top: 8px;
+                left: 8px;
+                color: black;
+                z-index: -1;
+
+            }
+        }
+
+        div.search input {
+            font-size: 2rem;
+            margin: 0 1rem;
+            outline: none;
+            background-color: transparent;
+            border: none;
+            border-bottom: 2px solid white;
+            color: white;
+            transition: .4s;
+
+            &:focus {
+                border-color: red;
+            }
+        }
+
+
+        div.search i {
+            color: white;
+            display: inline-block;
+            margin-right: 2rem;
+            font-size: 2rem;
+            cursor: pointer;
+            transition: .4s;
+
+            &:hover {
+                color: red;
+            }
+        }
+    }
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,12 @@
 <template>
     <main>
-        <h2>film</h2>
+        <h2 class="film-wrapper" v-if="films.length > 2">films</h2>
         <div class="cards-container">
             <CardElement v-for="film in films" :key="film.id"
             :item="film" />
         </div>
 
-        <h2>serie tv</h2>
+        <h2 class="tv-show-wrapper" v-if="TVSeries.length > 0">serie tv</h2>
         <div class="cards-container">
             <CardElement v-for="show in TVSeries" :key="show.id"
             :item="show" />
@@ -39,11 +39,50 @@ export default {
 
 <style lang="scss" scoped>
     
+    h2 {
+        text-transform: uppercase;
+        margin-left: 2rem;
+        font-size: 3rem;
+        color: red;
+        position: relative;
+
+        &.film-wrapper {
+            margin-top: 1rem;
+        }
+
+        &.film-wrapper::after {
+            content: "films";
+            text-transform: uppercase;
+            position: absolute;
+            top: 8px;
+            left: 8px;
+            color: black;
+            z-index: -1;
+        }
+
+        &.tv-show-wrapper::after {
+            content: "serie tv";
+            text-transform: uppercase;
+            position: absolute;
+            top: 8px;
+            left: 8px;
+            color: black;
+            z-index: -1;
+        }
+
+    }
+
     div.cards-container {
         margin: 2rem;
         overflow-x: auto;
         overflow-y: hidden;
         display: flex;
+
+        &::-webkit-scrollbar {
+            display: none;
+        }
     }
+
+
 
 </style>
