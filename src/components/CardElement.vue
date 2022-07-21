885df57129e31b10c2ff8831e1d792d5f1d1719e
DanCan30 <danielecan3008@gmail.com>
2022-07-21 15:42:27 +0200

    replaced original language field with flags and managed some issues with the flags API

--- src/components/CardElement.vue
+++ src/components/CardElement.vue
@@ -7,24 +7,66 @@
     <ul v-if="!film.title.toLowerCase().includes('undefined')">
         <li>Titolo: {{ film.title }}</li>
         <li>Titolo Originale: {{ film.original_title }} </li>
-        <li>Lingua Originale: {{ film.original_language }} </li>
+        <li>Lingua Originale: <img :src="flagUrl" :alt="film.original_language"></li>
         <li>Voto: {{ film.vote_average }} </li>
     </ul>
   </div>
 </template>
 
 <script>
+
+import axios from "axios";
+
 export default {
+
+    data: function() {
+        return {
+            flagAPIUrl: "https://countryflagsapi.com/png/",
+            flagUrl: "",
+        }
+    },
 
     props: {
         film: {
             required: true,
             type: Object,
         }
+    },
+
+    methods: {
+        getLanguageFlag: function() {
+            axios.get(`${this.flagAPIUrl}${this.film.original_language}`)
+            .then((result) => {
+
+                    this.flagUrl = result.request.responseURL;
+
+                    switch(this.flagUrl) {
+                    case 'https://countryflagsapi.com/png/en':
+                        this.flagUrl = 'https://countryflagsapi.com/png/gb';
+                        break;
+                    case 'https://countryflagsapi.com/png/zh':
+                        this.flagUrl = 'https://countryflagsapi.com/png/cn';
+                        break;
+                    case 'https://countryflagsapi.com/png/ja':
+                        this.flagUrl = 'https://countryflagsapi.com/png/jp';
+                        break;
+                    default:
+                        this.flagUrl = result.request.responseURL;
+                    }
+                }
+            )
+
+        },
+    },
+
+    created() {
+        this.getLanguageFlag();
     }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    img {
+        width: 32px;
+    }
 </style>
